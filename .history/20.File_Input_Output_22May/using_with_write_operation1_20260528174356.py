dbe9b04fbe96fe77fa922b64b081bb39ccb8f72b
Smruti <gondkarsmruti@gmail.com>
2026-05-28 17:50:07 +0530

    PYTHON-COURSE

---
+++ .history/20.File_Input_Output_22May/using_with_write_operation1_20260528174356.py
@@ -0,0 +1,3 @@
+with open("diary.txt","w") as fobj:
+    fobj.write("Day 3 - String")
+    fobj.write("Day 2 - List")
---
+++ .history/20.File_Input_Output_22May/using_with_write_operation1_20260528174417.py
@@ -0,0 +1,3 @@
+with open("diary.txt","w") as fobj:
+    fobj.write("Day 3 - ")
+    fobj.write("Day 4 - List")
