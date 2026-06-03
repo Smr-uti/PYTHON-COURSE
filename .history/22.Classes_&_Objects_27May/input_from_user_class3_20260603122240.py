53d8082311f9cae409e5c50c88faab5779699e0a
smruti <gondkarsmruti@gmail.com>
2026-06-03 08:22:41 +0000

    PYTHON-COURSE

---
+++ .history/22.Classes_&_Objects_27May/input_from_user_class3_20260603122240.py
@@ -0,0 +1,30 @@
+print("Start Program")
+
+class Vehicle:
+    def __init__(self, colour, top_speed, mileage):
+        self.colour = colour
+        self.top_speed = top_speed
+        self.mileage =mileage
+
+    def display_info(self):
+        print("Mileage:", self.mileage)
+        print("Top Speed:", self.top_speed)
+
+colour = input("Enter the colour of the vehicle: ")
+top_speed = int(input("Enter the top speed of the vehicle: "))      
+
+obj1 = Vehicle("Red", 200, 25)
+print(obj1.colour)
+print(obj1.top_speed)
+print(obj1.mileage) 
+
+obj1.display_info()
+
+obj2 = Vehicle("White", 220, 30)
+print(obj2.colour)
+print(obj2.top_speed)
+print(obj2.mileage)
+
+obj2.display_info()
+
+print("End Program")
