078578f42c821b82989ba7e3e5bade43f5e10e0a
smruti <gondkarsmruti@gmail.com>
2026-06-15 05:44:06 +0000

    PYTHON-COURSE

---
+++ 25.OOPS_Inheritance_30May/Private_Member_Access.py
@@ -0,0 +1,21 @@
+# child can't access private members of the class
+
+class Phone:
+    def __init__(self, price, brand, camera):
+        print ("Inside phone constructor")
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+    #getter
+    def show(self):
+        print (self.__price)
+
+class SmartPhone(Phone):
+    def check(self):
+        #print(self.__price)
+        print(self.brand)
+
+s=SmartPhone(20000, "Apple", 13)
+s.show()
+s.check()
